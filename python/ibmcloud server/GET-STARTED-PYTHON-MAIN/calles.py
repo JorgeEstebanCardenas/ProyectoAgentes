01956217a9c29e0bc78faea736b7381ed1c200c6
JorgeEstebanCardenas <A00232305@itesm.mx>
2022-09-06 16:33:22 -0700

    No dibujar calles

--- python/ibmcloud server/GET-STARTED-PYTHON-MAIN/calles.py
+++ python/ibmcloud server/GET-STARTED-PYTHON-MAIN/calles.py
@@ -1,5 +1,4 @@
-import pygame
-from pygame import gfxdraw
+
 import numpy as np
 import json
 from search import dijkstra_search
@@ -34,17 +33,7 @@ class Window:
 
     def loop(self, loop=None):
         """Shows a window visualizing the simulation and runs the loop function."""
-        
-        # Create a pygame window
-        self.screen = pygame.display.set_mode((self.width, self.height))
-        pygame.display.flip()
 
-        # Fixed fps
-        clock = pygame.time.Clock()
-
-        # To draw text
-        pygame.font.init()
-        self.text_font = pygame.font.SysFont('Lucida Console', 16)
 
         # Draw loop
         running = True
@@ -55,25 +44,16 @@ class Window:
             if self.sim.frame_count >10000:
                 running = False
 
-            # Draw simulation
             self.draw()
 
-            # Update window
-            pygame.display.update()
-            clock.tick(self.fps)
 
-            # Handle all events
-            for event in pygame.event.get():
-                # Quit program if window is closed
-                if event.type == pygame.QUIT:
-                    running = False
                     
             self.step += 1
 
         with open("anim.json","w") as outfile:
             outfile.write(json.dumps(self.sim.anim))
 
-        pygame.quit()
+        # pygame.quit()
         
     def run(self, steps_per_update=1):
         """Runs the simulation by updating in every loop."""
@@ -103,123 +83,9 @@ class Window:
             int(-self.offset[1] + (y - self.height/2)/self.zoom)
         )
 
-    def background(self, r, g, b):
-        """Fills screen with one color."""
-        self.screen.fill((r, g, b))
+ 
 
-    def line(self, start_pos, end_pos, color):
-        """Draws a line."""
-        gfxdraw.line(
-            self.screen,
-            *start_pos,
-            *end_pos,
-            color
-        )
-
-    def rect(self, pos, size, color):
-        """Draws a rectangle."""
-        gfxdraw.rectangle(self.screen, (*pos, *size), color)
-
-    def box(self, pos, size, color):
-        """Draws a rectangle."""
-        gfxdraw.box(self.screen, (*pos, *size), color)
-
-    def circle(self, pos, radius, color, filled=True):
-        gfxdraw.aacircle(self.screen, *pos, radius, color)
-        if filled:
-            gfxdraw.filled_circle(self.screen, *pos, radius, color)
-
-    def polygon(self, vertices, color, filled=True):
-        gfxdraw.aapolygon(self.screen, vertices, color)
-        if filled:
-            gfxdraw.filled_polygon(self.screen, vertices, color)
-
-    def rotated_box(self, pos, size, angle=None, cos=None, sin=None, centered=True, color=(0, 0, 255), filled=True):
-        """Draws a rectangle center at *pos* with size *size* rotated anti-clockwise by *angle*."""
-        x, y = pos
-        l, h = size
-
-        if angle:
-            cos, sin = np.cos(angle), np.sin(angle)
-        
-        vertex = lambda e1, e2: (
-            x + (e1*l*cos + e2*h*sin)/2,
-            y + (e1*l*sin - e2*h*cos)/2
-        )
-
-        if centered:
-            vertices = self.convert(
-                [vertex(*e) for e in [(-1,-1), (-1, 1), (1,1), (1,-1)]]
-            )
-        else:
-            vertices = self.convert(
-                [vertex(*e) for e in [(0,-1), (0, 1), (2,1), (2,-1)]]
-            )
-
-        self.polygon(vertices, color, filled=filled)
-
-    def rotated_rect(self, pos, size, angle=None, cos=None, sin=None, centered=True, color=(0, 0, 255)):
-        self.rotated_box(pos, size, angle=angle, cos=cos, sin=sin, centered=centered, color=color, filled=False)
-
-    def arrow(self, pos, size, angle=None, cos=None, sin=None, color=(150, 150, 190)):
-        if angle:
-            cos, sin = np.cos(angle), np.sin(angle)
-        
-        self.rotated_box(
-            pos,
-            size,
-            cos=(cos - sin) / np.sqrt(2),
-            sin=(cos + sin) / np.sqrt(2),
-            color=color,
-            centered=False
-        )
-
-        self.rotated_box(
-            pos,
-            size,
-            cos=(cos + sin) / np.sqrt(2),
-            sin=(sin - cos) / np.sqrt(2),
-            color=color,
-            centered=False
-        )
-
-    def draw_axes(self, color=(100, 100, 100)):
-        x_start, y_start = self.inverse_convert(0, 0)
-        x_end, y_end = self.inverse_convert(self.width, self.height)
-        self.line(
-            self.convert((0, y_start)),
-            self.convert((0, y_end)),
-            color
-        )
-        self.line(
-            self.convert((x_start, 0)),
-            self.convert((x_end, 0)),
-            color
-        )
-
-    def draw_grid(self, unit=50, color=(150,150,150)):
-        x_start, y_start = self.inverse_convert(0, 0)
-        x_end, y_end = self.inverse_convert(self.width, self.height)
-
-        n_x = int(x_start / unit)
-        n_y = int(y_start / unit)
-        m_x = int(x_end / unit)+1
-        m_y = int(y_end / unit)+1
-
-        for i in range(n_x, m_x):
-            self.line(
-                self.convert((unit*i, y_start)),
-                self.convert((unit*i, y_end)),
-                color
-            )
-        for i in range(n_y, m_y):
-            self.line(
-                self.convert((x_start, unit*i)),
-                self.convert((x_end, unit*i)),
-                color
-            )
-
-    def draw_semaforos(self, screen, frame_count):
+    def draw_semaforos(self, frame_count):
 
         for road in self.sim.roads:
 
@@ -246,14 +112,6 @@ class Window:
                     road.semaforo.cambiar_estado()
 
                 color = road.semaforo.colores[road.semaforo.estado_actual]
-                self.rotated_box(
-                    pos, 
-                    (4, 5), 
-                    cos=road.angle_cos,
-                    sin=road.angle_sin,
-                    color=color, 
-                    filled=True
-                )
 
 
                 agent = {
@@ -281,8 +139,6 @@ class Window:
             l=4
             x = self.sim.roads[roadindex].start[0] + cos * carro.pos
             y = self.sim.roads[roadindex].start[1] + sin * carro.pos 
-
-            self.rotated_box((x, y), (l, h), cos=cos, sin=sin, centered=True)
 
             alpha = 0
 
@@ -336,30 +192,7 @@ class Window:
 
     def draw_roads(self, screen):
         for road in self.sim.roads:
-            # Draw road background
-            self.rotated_box(
-                road.start,
-                (road.length, 3.7),
-                cos=road.angle_cos,
-                sin=road.angle_sin,
-                color=(180, 180, 220),
-                centered=False
-            )
 
-            
-
-
-            # Draw road lines
-            # self.rotated_box(
-            #     road.start,
-            #     (road.length, 0.25),
-            #     cos=road.angle_cos,
-            #     sin=road.angle_sin,
-            #     color=(0, 0, 0),
-            #     centered=False
-            # )
-
-            # Draw road arrow
             if road.length > 5: 
                 for i in np.arange(-0.5*road.length, 0.5*road.length, 10):
                     pos = (
@@ -367,39 +200,13 @@ class Window:
                         road.start[1] + (road.length/2 + i + 3) * road.angle_sin
                     )
 
-                    self.arrow(
-                        pos,
-                        (-1.25, 0.2),
-                        cos=road.angle_cos,
-                        sin=road.angle_sin
-                    )   
             
-    def draw_status(self):
-        text_fps = self.text_font.render(f't={self.sim.t:.5}', False, (0, 0, 0))
-        text_frc = self.text_font.render(f'n={self.sim.frame_count}', False, (0, 0, 0))
-        
-        self.screen.blit(text_fps, (0, 0))
-        self.screen.blit(text_frc, (100, 0))
+
 
     def draw(self):
-        # Fill background
-        self.background(*self.bg_color)
-
-        # Major and minor grid and axes
-        self.draw_grid(10, (220,220,220))
-        self.draw_grid(100, (200,200,200))
-        self.draw_axes()
-
-        self.draw_roads(self.screen)
-        #self.draw_vehicles()
-        #self.draw_signals()
-
-        # Draw status info
-        self.draw_status()
-
         self.draw_carro()
         
-        self.draw_semaforos(self.screen, self.sim.frame_count)
+        self.draw_semaforos(self.sim.frame_count)
 
 
 
